375eeced3021fbf02b3c6ebf680499f40bdc64fe
Rubel2475 <37175213+Rubel2475@users.noreply.github.com>
2022-01-12 23:54:01 +0600

    app created

--- bookstore_database_using_PDO/database/connection.php
+++ bookstore_database_using_PDO/database/connection.php
@@ -3,9 +3,9 @@
     //$dbConfig = config::getInstance()->get('db');
     function connect(){
         $db = new PDO(
-            "mysql:host=127.0.0.1; dbname=PDO_bookstore",
-            "root",
-            ""
+            "mysql:host=ftpupload.net; dbname=PDO_bookstore",
+            "epiz_30799275",
+            "rubelinfinity"
         );
         
         $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
@@ -15,11 +15,5 @@
         }
         return $db;
     }
-
-    // $data = $db->query("SELECT * FROM `book` WHERE 1");
-
-    // foreach($data as $key => $datum):
-    //     var_dump($datum);
-    // endforeach;
 
 ?>
